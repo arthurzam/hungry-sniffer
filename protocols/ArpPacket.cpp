740d6adb53987e52e1944e5e714ec7c5d1a16af1
Arthur Zamarin <arthurzam@gmail.com>
2014-12-06 08:36:30 +0200

    Finish Arp Packet

--- protocols/ArpPacket.cpp
+++ protocols/ArpPacket.cpp
@@ -1,5 +1,4 @@
 #include "ArpPacket.h"
-#include <arpa/inet.h>
 
 using namespace std;
 
@@ -7,5 +6,33 @@ using namespace std;
 ArpPacket::ArpPacket(const void* data, size_t len, const Protocol* protocol, const Packet* prev)
     : PacketStructed(data, len, protocol, prev)
 {
-    this->headers.push_back({"type", (ntohs(this->value.ar_op) == ARPOP_REQUEST ? "Request" : "Reply")});
+    this->headers.push_back({"Hardware Type", std::to_string(ntohs(this->value.ar_hrd))});
+    this->headers.push_back({"Protocol Type", std::to_string(ntohs(this->value.ar_pro))});
+    this->headers.push_back({"Hardware Size", std::to_string(this->value.ar_hln)});
+    this->headers.push_back({"Protocol Size", std::to_string(this->value.ar_pln)});
+
+    this->headers.push_back({"Type", (ntohs(this->value.ar_op) == ARPOP_REQUEST ? "Request" : "Reply")});
+
+    if(ntohs(this->value.ar_hrd) == 1 && (ntohs(this->value.ar_pro) == 0x0800 || ntohs(this->value.ar_pro) == 0x86dd))
+    {
+        memcpy(&this->data, (const char*)data + sizeof(value),
+                (ntohs(this->value.ar_pro) == 0x0800 ? sizeof(this->data.eth_ip) : sizeof(this->data.eth_ipv6)));
+
+        this->headers.push_back({"Sender MAC Address", ether_ntoa((struct ether_addr*) this->data.eth_ip.arp_sha)});
+
+        char str[INET6_ADDRSTRLEN];
+        if(ntohs(this->value.ar_pro) == 0x0800)
+            inet_ntop(AF_INET, &this->data.eth_ip.arp_sip, str, INET6_ADDRSTRLEN);
+        else
+            inet_ntop(AF_INET6, &this->data.eth_ipv6.arp_sip, str, INET6_ADDRSTRLEN);
+        this->headers.push_back({"Sender IP Address" , str});
+
+        this->headers.push_back({"Target MAC Address", ether_ntoa((struct ether_addr*) this->data.eth_ip.arp_tha)});
+
+        if(ntohs(this->value.ar_pro) == 0x0800)
+            inet_ntop(AF_INET, &this->data.eth_ip.arp_tip, str, INET6_ADDRSTRLEN);
+        else
+            inet_ntop(AF_INET6, &this->data.eth_ipv6.arp_tip, str, INET6_ADDRSTRLEN);
+        this->headers.push_back({"Target IP Address" , str});
+    }
 }
--- protocols/ArpPacket.h
+++ protocols/ArpPacket.h
@@ -3,13 +3,32 @@
 
 #include "Protocol.h"
 #include <net/if_arp.h>
-#include <iostream>
+#include <arpa/inet.h>
+#include <netinet/ether.h>
 
 using namespace hungry_sniffer;
 
 class ArpPacket : public PacketStructed<struct arphdr> {
+    private:
+        union {
+            struct {
+                uint8_t arp_sha[ETH_ALEN]; /* Sender hardware address.  */
+                uint8_t arp_sip[4]; /* Sender IP address.  */
+                uint8_t arp_tha[ETH_ALEN]; /* Target hardware address.  */
+                uint8_t arp_tip[4]; /* Target IP address.  */
+            } eth_ip;
+
+            struct {
+                uint8_t arp_sha[ETH_ALEN]; /* Sender hardware address.  */
+                struct in6_addr arp_sip; /* Sender IP address.  */
+                uint8_t arp_tha[ETH_ALEN]; /* Target hardware address.  */
+                struct in6_addr arp_tip; /* Target IP address.  */
+            } eth_ipv6;
+        } data;
+
     public:
-        ArpPacket(const void* data, size_t len, const Protocol* protocol, const Packet* prev);
+        ArpPacket(const void* data, size_t len, const Protocol* protocol,
+                const Packet* prev);
 
         virtual ~ArpPacket() {}
 };
