e7ff701c00ed25e87cf5d1d6f25991b4cb58bce2
Arthur Zamarin <arthurzam@gmail.com>
2015-05-07 16:23:58 +0300

    Add support for args
    quiet mode - no output
    load pcaps transferred as arguments

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include <QDir>
 #include <sys/time.h>
 #include <sys/resource.h>
+#include <unistd.h>
 
 #ifndef PLUGINS_DIRECTORY
 #define PLUGINS_DIRECTORY "/home/arthur/QT/build-hungry-sniffer-Desktop-Debug/plugins/"
@@ -41,17 +42,36 @@ static void setMaxRam()
 
 int main(int argc, char *argv[])
 {
+    std::vector<std::string> files;
+    files.reserve(argc - 1);
+    for(int i = 1; i < argc; i++)
+    {
+        if(argv[i][0] == '-')
+        {
+            if(strcmp(argv[i], "-quiet") == 0)
+            {
+                ::close(STDOUT_FILENO);
+                ::close(STDERR_FILENO);
+            }
+        }
+        else
+        {
+            files.push_back(argv[i]);
+        }
+    }
     setMaxRam();
+
     hungry_sniffer::Protocol base(hungry_sniffer::init<EthernetPacket>, true, "Ethernet", true, true);
     base.addFilter("^dst *== *([^ ]+)$", EthernetPacket::filter_dstMac);
     base.addFilter("^src *== *([^ ]+)$", EthernetPacket::filter_srcMac);
-
     SniffWindow::core = new HungrySniffer_Core(base);
 
     loadLibs();
 
     QApplication a(argc, argv);
     SniffWindow w;
+    for(const auto& i : files)
+        w.runOfflinePcap(i);
     w.show();
     return a.exec();
 }
